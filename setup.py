4e7068aedf2f794c78972612db2d7f83d7c58008
Simona Bernardi <simonab@unizar.es>
2022-05-16 11:16:55 +0200

    update

--- setup.py
+++ setup.py
@@ -5,8 +5,22 @@ setup(
     packages=find_packages(include=['tegdet']),
     version='2.0.0',
     description='Time Evolving Graph detectors library',
+    long_description="README.md",
+    long_description_content_type="text/markdown",
+    url="https://github.com/DiasporeUnizar/TEG",
     author='Diaspore team',
+    author_email="simonab@unizar.es",
     license='GPL-2',
+    classifiers=[
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: 3.10",
+    "Operating System :: OS Independent"
+    ],
     install_requires=[
         'numpy>=1.14.5',
         'pandas>=1.1',
@@ -15,5 +29,5 @@ setup(
     python_requires='>=3.6.1',
     setup_requires=['pytest-runner'],
     tests_require=['pytest>=6.2.2'],
-    test_suite='test',
+    test_suite='test'
 )
